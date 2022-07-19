c573e54c5d496faf136157cb8bac2b893b37c4f6
Thibault Berthelin <61558171+DevoWeb60@users.noreply.github.com>
2022-07-20 00:54:14 +0200

    database full and end soon

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,9 @@ const API_URL = "http://devoweb-tools.test/api";
 
 function App(data) {
     const container = document.querySelector(".container");
+    const categoryContainer = document.querySelector(".category-container");
+    const toolsContainer = document.querySelector(".tools-container");
+    const backButton = document.querySelector("#back");
 
     data.categories.forEach((category) => {
         const div = document.createElement("div");
@@ -11,7 +14,7 @@ function App(data) {
         div.innerHTML = `<h2><img src="${category.icon}">${category.name}</h2>`;
         data.subcategories.forEach((subcategory) => {
             if (subcategory.parent_category == category.id) {
-                div.innerHTML += `<h3 class="subcategory sub-link" id="sub-${subcategory.id}">${subcategory.name} <i class="fas fa-long-arrow-alt-right"></i></h3>`;
+                div.innerHTML += `<h3 class="subcategory sub-link" data-cat="${category.id}" data-subid="${subcategory.id}">${subcategory.name} <i class="fas fa-long-arrow-alt-right"></i></h3>`;
             }
         });
         data.tools.forEach((tool) => {
@@ -19,10 +22,50 @@ function App(data) {
                 tool.category_id == category.id &&
                 tool.subcategory_id == null
             ) {
-                div.innerHTML += `<a href="${tool.link}" class="sub-link" target="_blank">${tool.name}</a>`;
+                div.innerHTML += `<a href="${tool.link}" class="sub-link" target="_blank">
+                <h3>${tool.name}</h3>
+                <p>${tool.description}</p>
+                </a>`;
             }
         });
-        container.appendChild(div);
+        categoryContainer.appendChild(div);
+    });
+
+    const subCategories = document.querySelectorAll(".subcategory");
+    subCategories.forEach((subcategory) => {
+        subcategory.addEventListener("click", () => {
+            const categoryId = subcategory.dataset.cat;
+            const subcategoryId = subcategory.dataset.subid;
+
+            const div = document.createElement("div");
+            div.classList.add("tools-lists");
+            data.categories.forEach((category) => {
+                if (category.id == categoryId) {
+                    data.subcategories.forEach((subcategory) => {
+                        if (subcategory.id == subcategoryId) {
+                            data.tools.forEach((tool) => {
+                                if (tool.subcategory_id == subcategoryId) {
+                                    div.innerHTML += `
+                                    <a href="${tool.link}" class="sub-link" target="_blank">
+                                        <h3><img src="${tool.favicon}">${tool.name}</h3>
+                                        <p>${tool.description}</p>
+                                    </a>`;
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+            toolsContainer.appendChild(div);
+
+            container.classList.add("active");
+        });
+    });
+
+    backButton.addEventListener("click", () => {
+        container.classList.remove("active");
+        const toolsList = toolsContainer.querySelector(".tools-lists");
+        toolsList.remove();
     });
 }
 
